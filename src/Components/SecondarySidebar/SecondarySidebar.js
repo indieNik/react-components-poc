6bfd346d809e3c1ddef5ca5b1af7a7e9055e5538
Nikhil Patil <nikhil@engagedly.com>
2019-02-26 19:15:16 +0530

    Added Header, Sidebar and SecondarySidebar Components with skeleton structure

---
+++ src/Components/SecondarySidebar/SecondarySidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './SecondarySidebar.scss';
+
+const SecondarySidebar = () => {
+    return <div className="SecondarySidebar"></div>
+}
+
+export default SecondarySidebar;
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Sidebar.scss';
+
+const Sidebar = () => {
+    return <div className="Sidebar"></div>
+}
+
+export default Sidebar;
